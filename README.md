5b5c36bf22e629a584ba92a6754e06af6d9f8b10
HAnthony007 <harifetraanthonyr@gmail.com>
2024-12-03 00:30:53 +0300

    Guessing game successfuly!

--- README.md
+++
@@ -1,2 +0,0 @@
-# Rust
-Learn rust Doc
---
+++ project/guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
+
+fn main() {
+    println!("Devinez le nombre !");
+
+    let nombre_secret = rand::thread_rng().gen_range(1..=100);
+    // println!("Le nombre secret est: {}", nombre_secret);
+
+    loop {
+        println!("Veuillez entrer un nombre: ");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Echec de la lecture de la ligne");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+        println!("Vous avez devine le nombre {}", guess);
+
+        match guess.cmp(&nombre_secret) {
+            Ordering::Less => println!("Trop petit !"),
+            Ordering::Greater => println!("Trop grand !"),
+            Ordering::Equal => {
+                println!("Vous avez gagne !");
+                break;
+            }
+        }
+    }
+}
