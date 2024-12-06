70086406339f54128aa9ebb486ecdc4885f604f4
HAnthony007 <harifetraanthonyr@gmail.com>
2024-12-06 21:08:39 +0300

    variables finished

--- concepts/variables/src/main.rs
+++ concepts/variables/src/main.rs
@@ -1,3 +1,30 @@
+// // Constants
+// const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, variable!")
+    // // Variables immutable
+    // let mut x = 5;
+    // println!("La valeur de x est {x}");
+    // x = 6;
+    // println!("La valeur de x est {x}");
+    // // Cannot change type of variable
+    // let mut first_spaces = "   ";
+    // first_spaces = first_spaces.len();
+    // println!("Number of space: {first_spaces}");
+
+    // // Constants
+    // println!("La valeur du constante Max points: {MAX_POINTS}")
+
+    // // Shadowing
+    let x = 5;
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("La valeur de x dans le block interne est: {x}");
+    }
+    println!("La valeur de x est: {x}");
+    // Can change the type of the variable
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("Number of space: {spaces}")
 }
