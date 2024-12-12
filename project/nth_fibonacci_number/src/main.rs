f7b20710b1e85153dcc7252f642c05620c744c46
HAnthony007 <harifetraanthonyr@gmail.com>
2024-12-13 00:52:58 +0300

    Fiboacci standar finish

--- project/nth_fibonacci_number/src/main.rs
+++ project/nth_fibonacci_number/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, fibonacci!");
+    println!("fibonacci de 5 est {}", fibonacci(3));
+    println!("fibonacci de 6 est {}", fibonacci(6));
+    println!("fibonacci de 7 est {}", fibonacci(7));
+    println!("fibonacci de 8 est {}", fibonacci(8));
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
